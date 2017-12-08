cf1151044bfd2449ab7e0c6e8fb6ba1f8624e407
Diego Nuñez <diego.gabriel.nd@gmail.com>
2017-12-07 22:34:19 -0400

    seed with ticket numbers

--- gambi2018/db/seeds.rb
+++ gambi2018/db/seeds.rb
@@ -1,9 +1,9 @@
 User.create! username: "admin", password: "admin"
 
-(1..10).to_a.each do |id|
-	User.create! username: id, password: id
+(1..900).to_a.each do |id|
+	User.create! username: format("%05d", id), password: ""
 end 
 
-300.times do 
+300.times do
 	Table.create! status: Table::FREE
 end
